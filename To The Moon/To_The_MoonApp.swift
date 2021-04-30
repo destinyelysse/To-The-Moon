c85ca1a704d8fafbe78633eea49886fff5a03e38
Destiny Serna <destinyeserna@gmail.com>
2021-04-30 15:55:40 -0700

    Initial Commit

---
+++ To The Moon/To_The_MoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_The_MoonApp.swift
+//  To The Moon
+//
+//  Created by Destiny Serna on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct To_The_MoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
